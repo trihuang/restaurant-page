aa1e856501821631cb2f65070f902e5325127a8c
trihuang <124452877+trihuang@users.noreply.github.com>
2023-03-07 17:36:09 +0800

    Use JavaScript to dynamically populate the home page

---
+++ src/home.js
@@ -0,0 +1,53 @@
+function changeToHomeTab() {
+    const parentDiv = document.getElementById('content');
+
+    const photoDiv = document.createElement('div');
+    photoDiv.classList.add('container');
+    photoDiv.classList.add('img-container');
+    photoDiv.classList.add('position-fixed');
+    photoDiv.classList.add('d-flex');
+    photoDiv.classList.add('justify-content-center');
+    photoDiv.classList.add('justify-content-sm-start');
+    
+    const img = document.createElement('img');
+    img.src = '../src/img/coffee.jpg';
+    img.id = 'coffee';
+    img.alt = 'Photo of a coffee';
+    photoDiv.appendChild(img);
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('container');
+    textContainer.classList.add('text-center');
+
+    const name = document.createElement('div');
+    name.classList.add('text-warning');
+    name.classList.add('big');
+    name.classList.add('name');
+    name.textContent = 'Yummy Bistro';
+    textContainer.appendChild(name);
+
+    const textDiv = document.createElement('div');
+    textDiv.classList.add('text');
+    textDiv.classList.add('d-flex');
+    textDiv.classList.add('justify-content-center');
+    textDiv.classList.add('justify-content-sm-end');
+    textDiv.classList.add('mt-5');
+
+    const leadText = document.createElement('div');
+    leadText.classList.add('lead');
+    leadText.classList.add('text-light');
+    leadText.classList.add('about-us');
+
+    const displayText = document.createElement('div');
+    displayText.classList.add('display-4');
+    displayText.textContent = 'About Us';
+    leadText.appendChild(displayText);
+    leadText.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, architecto expedita iste a voluptatum saepe cupiditate inventore eos soluta, dignissimos adipisci iusto distinctio labore impedit ipsam quam quisquam possimus itaque perferendis sed reiciendis eveniet dicta neque? Sunt nostrum modi nemo mollitia, aut repudiandae, delectus eum rerum ea vitae, unde assumenda?';
+    textDiv.appendChild(leadText);
+    textContainer.appendChild(textDiv);
+
+    parentDiv.appendChild(photoDiv);
+    parentDiv.appendChild(textContainer);
+}
+
+export { changeToHomeTab };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,76 @@
 import './style.css';
+import { changeToHomeTab } from './home';
+
+const content = document.getElementById('content');
+const homeTab = document.querySelector('.home');
+const menuTab = document.querySelector('.menu');
+const contactTab = document.querySelector('.contact');
+const logo = document.querySelector('.logo');
+
+const tabs = ["Home", "Menu", "Contact"];
+let currentTab = tabs[0];
+
+homeTab.addEventListener('click', changeTab);
+menuTab.addEventListener('click', changeTab);
+contactTab.addEventListener('click', changeTab);
+logo.addEventListener('click', changeTab);
+
+function changeTab(e) {
+    if (e.target.classList.contains('home') && currentTab === tabs[0] || e.target.classList.contains('logo') && currentTab === tabs[0]) {
+        return;
+    } else if (e.target.classList.contains('menu') && currentTab === tabs[1]) {
+        return;
+    } else if (e.target.classList.contains('contact') && currentTab === tabs[2]) {
+        return;
+    } else if (e.target.classList.contains('home') || e.target.classList.contains('logo')) {
+        currentTab = tabs[0];
+        removeAllChildNodes(content);
+        updateTabs(currentTab);
+        changeToHomeTab();
+    } else if (e.target.classList.contains('menu')) {
+        currentTab = tabs[1];
+        removeAllChildNodes(content);
+        updateTabs(currentTab);
+        // Change to menu page
+        console.log('change to menu...');
+    } else if (e.target.classList.contains('contact')) {
+        currentTab = tabs[2];
+        removeAllChildNodes(content);
+        updateTabs(currentTab);
+        // Change to contact page
+        console.log('change to contact...');
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function updateTabs(current) {
+    if (current === tabs[0]) {
+        homeTab.classList.add('active');
+        homeTab.classList.remove('text-light');
+        menuTab.classList.remove('active');
+        menuTab.classList.add('text-light');
+        contactTab.classList.remove('active');
+        contactTab.classList.add('text-light');
+    } else if (current === tabs[1]) {
+        menuTab.classList.add('active');
+        menuTab.classList.remove('text-light');
+        homeTab.classList.remove('active');
+        homeTab.classList.add('text-light');
+        contactTab.classList.remove('active');
+        contactTab.classList.add('text-light');
+    } else if (current === tabs[2]) {
+        contactTab.classList.add('active');
+        contactTab.classList.remove('text-light');
+        homeTab.classList.remove('active');
+        homeTab.classList.add('text-light');
+        menuTab.classList.remove('active');
+        menuTab.classList.add('text-light');
+    }
+}
+
+changeToHomeTab();
